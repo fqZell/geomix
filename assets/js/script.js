e56ef516af12ca1c424539b6186d6bc8dc376e00
fqZell <mulkyway00@gmail.com>
2024-10-14 17:19:27 +0300

    add to tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,19 @@ const headerMenu = () => {
     const headerOverlay = document.querySelector(".overlay");
     const headerMenuWrapper = document.querySelector(".header__menu__wrapper");
 
-    // Показ меню при наведении на элемент меню
     headerEl.addEventListener("mouseover", () => {
         headerOverlay.classList.add("active");
         headerMenuEl.classList.add("active");
         document.body.classList.add("active-menu");
         
-        // Добавляем класс для поворота стрелки
         headerEl.classList.add("rotate-arrow");
     });
 
-    // Закрытие меню при уходе курсора с wrapper'а
     headerMenuWrapper.addEventListener("mouseleave", () => {
         headerOverlay.classList.remove("active");
         headerMenuEl.classList.remove("active");
         document.body.classList.remove("active-menu");
         
-        // Убираем класс при выходе курсора
         headerEl.classList.remove("rotate-arrow");
     });
 };
@@ -39,9 +35,35 @@ const sliderBanner = () => {
     });
 };
 
+const tabs = () => {
+    const tabsEl = document.querySelectorAll(".product__column__header");
+    const tabContentsEl = document.querySelectorAll(".product__tab");
+
+    tabsEl.forEach((tabEl, index) => {
+        tabEl.addEventListener("click", () => {
+            // Удаляем класс "active" у всех заголовков вкладок
+            tabsEl.forEach((el) => {
+                el.classList.remove("active");
+            });
+            
+            // Добавляем класс "active" к текущему заголовку вкладки
+            tabEl.classList.add("active");
+
+            // Удаляем класс "active" у всех содержимых вкладок
+            tabContentsEl.forEach((content) => {
+                content.classList.remove("active");
+            });
+            
+            // Добавляем класс "active" только к соответствующему содержимому вкладки
+            tabContentsEl[index].classList.add("active");
+        });
+    });
+};
+
 const init = () => {
     headerMenu()
     sliderBanner()
+    tabs()
 };
 
 document.addEventListener('DOMContentLoaded', init);
